349fb2d63e64ee755ab85e27b544819db3757930
blissofever <blissofever@hotmail.com>
2025-01-06 01:01:24 +0900

    link, map, html change

--- js/common.js
+++ js/common.js
@@ -52,7 +52,7 @@ $(function(){
 
 	$('.schedule>.btn-box .btn.info').on('click', function(e){
 		e.preventDefault();
-		window.open('https://puddle-bovid-427.notion.site/10f89220101b80949cecc78e1f6f5f65', '_blank');
+		window.open('https://visit-europe.notion.site/10f89220101b80949cecc78e1f6f5f65?pvs=4', '_blank');
         });
 
 });
