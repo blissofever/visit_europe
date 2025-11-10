0beec57a82763b3393e4bb97f5909446c48ffb80
blissofever <blissofever@hotmail.com>
2025-11-10 23:03:19 +0900

    ban list update

--- netlify/functions/saveUserInfo.js
+++ netlify/functions/saveUserInfo.js
@@ -2,24 +2,25 @@
 
 const bannedUsers = ["123456789", "987654321",
   
-  // "3925395335", //cthinae@naver.com
-  // "3930863609", //58827099a@gmail.com
-  // "3935150212", //jjb_0705@naver.com
-  // "3938203995", //hw01619@naver.com
-  // "3959513059", //da6987@naver.com
-  // "3917592151", //knighta@naver.com
-  // "4229162689", //skagus7912@naver.com
-  // "4230780696", //paradise2151@naver.com
-  // "4242974826", //qotpdl20@nate.com
-  // "4215931465", //chaos132@naver.com
-  // "4323673373", //chingoo4860@naver.com
-  // "3919632115", //jan4081@naver.com
-  // "3919437960", //7116420@hanmail.net
-  // "3929517577", //mansaok79@hanmail.net
-  // "4209342117", //sunny-family@nate.com
-  // "4227462736", //dmzgt@naver.com
-  // "3919380490", //bb9706@daum.net
-  "3900898788", //blissofever@hotmail.com
+  "3925395335", //cthinae@naver.com
+  "3930863609", //58827099a@gmail.com
+  "3935150212", //jjb_0705@naver.com
+  "3938203995", //hw01619@naver.com
+  "3959513059", //da6987@naver.com
+  "3917592151", //knighta@naver.com
+  "4229162689", //skagus7912@naver.com
+  "4230780696", //paradise2151@naver.com
+  "4242974826", //qotpdl20@nate.com
+  "4215931465", //chaos132@naver.com
+  "4323673373", //chingoo4860@naver.com
+  "3919632115", //jan4081@naver.com
+  "3919437960", //7116420@hanmail.net
+  "3929517577", //mansaok79@hanmail.net
+  "4209342117", //sunny-family@nate.com
+  "4227462736", //dmzgt@naver.com
+  "3919380490", //bb9706@daum.net
+  "3916838698", //selfh1@naver.com
+  // "3900898788", //blissofever@hotmail.com
 ];
 
 exports.handler = async function(event, context) {
